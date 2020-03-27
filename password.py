579525dabd7f3525ceb6905a5d109f6bd69b915a
sunnie1239 <sunnie1239@gmail.com>
2020-03-26 18:53:27 -0700

    Update while sentence

--- password.py
+++ password.py
@@ -1,7 +1,7 @@
 password = 'a123456'
 x = 2
 
-while True:
+while x >= 0:
 	user_insert = input('請輸入密碼:')
 	if user_insert == password :
 		print('登入成功!!')
@@ -9,5 +9,3 @@ while True:
 	else :
 		print('密碼錯誤!! 還有', x, '機會')
 		x = x - 1
-		if x < 0 :
-			break
